6263b198c81e3ce89c719572d92acf7663a0fdfa
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2015-02-15 17:51:54 +0000

    Update init boot  : mount correctly btfs partition at startup

--- recipes-core/initrdscripts/files/init-boot.sh
+++ recipes-core/initrdscripts/files/init-boot.sh
@@ -10,25 +10,41 @@ mount -t sysfs sysfs /sys
 mknod -m 600 /dev/loop0 b 7 0
 mknod -m 600 /dev/loop1 b 7 1
 
+# mount sdcard
 mkdir -p /mnt/boot
 mount /dev/mmcblk0p1 /mnt/boot
-losetup /dev/loop0 /mnt/boot/calaos-os.btrfs
+
+#Mount current calaos-os-system partition
+if [ ! -f /mnt/boot/calaos-os-system.btrfs ]; then
+    echo "Unable to find calaos-os-system.btrfs, launching rescue shell ..."
+    exec /bin/sh
+fi
+
+losetup /dev/loop0 /mnt/boot/calaos-os-system.btrfs
 btrfstune -S 1 /dev/loop0
 mkdir /mnt/calaos-os
 mount /dev/loop0 /mnt/calaos-os
 
-if [ -! -f /mnt/boot/calaos-os-user.btrfs ]; then
-    dd if=/dev/zero of=/mnt/boot/calaos-os-user.btrfs count=1 bs=500M
-    losetup /dev/loop1 /mnt/boot/calaos-os-user.btrfs
+#Mount calaos-os-user partition
+if [ ! -f /mnt/boot/calaos-os-user.btrfs ]; then
+    # Partition doesn't exists, create a new one
+    dd if=/dev/zero of=/mnt/boot/calaos-os-user.btrfs count=1 bs=100M
+    losetup /dev/loop1 /mnt/boot/calaos-os-user.btrfs 
+    # Format with btrfs format
     mkfs.btrfs /dev/loop1
-elif
+else
     losetup /dev/loop1 /mnt/boot/calaos-os-user.btrfs
 fi
 
-btrfs device add /dev/loop1 /mnt/calaos-os
+# Add user partition to the main RO calaos-system partition
+btrfs device add /dev/loop1 /mnt/calaos-os -f
+
+# Remount the whole partition in RW 
 mount -o remount,rw /mnt/calaos-os
 
+# Switch root
 cd /mnt/calaos-os
-switch_root . /bin/init
+exec switch_root . /sbin/init
 
-exec sh
+echo "Unable to switch root and boot calaos-os system, launching rescue shell ..."
+exec /bin/sh
